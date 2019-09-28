43b543fac03f8f555115bc1e163fbe3e756b1be7
swapinal.j <swapinal.j@BBMQASWAPINALJ.local>
2019-09-29 00:52:21 +0530

    Adding somthings in GetOTP

--- src/main/java/com/tcp/tcpApplication/TheCenterPointApplication.java
+++ src/main/java/com/tcp/tcpApplication/TheCenterPointApplication.java
@@ -3,10 +3,6 @@ package com.tcp.tcpApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.tcp"})
--- src/main/java/com/tcp/tcpController/TCPController.java
+++ src/main/java/com/tcp/tcpController/TCPController.java
@@ -1,25 +1,33 @@
 package com.tcp.tcpController;
 
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tcp.servicesStore.ShopWithoutMenu;
 
 @RestController
 public class TCPController {
 	
 	@RequestMapping(value = "/getOTP", method = RequestMethod.GET)
-	public String getOTPforUser(@RequestParam(name = "mn", required = true) long mobileNumber) {
-		return "Ur mobile no is "+ mobileNumber +" And OTP is " +1234;
+	public void getOTPforUser(@RequestParam(name = "mobileNumber", required = true) long mobileNumber) {
+		int otp=0;
+		// TODO: Check if the OTP is available in Cache Memory if yes then use the same 
+		
+		// TODO: Check if the OTP is available in Cache Memory if No then Check the OTP in User DB and if available then use the same
+		
+		// TODO: Check if the OTP is available in Cache Memory if No then Check the OTP in User DB and if not available then generate the OTP
+		SecureRandom random = new SecureRandom(); 
+        otp = random.nextInt(10000)*random.nextInt(10000)/random.nextInt(10000);
+        
+        //TODO:Saving the OTP in Cache and Pushing an Kafaka event So the In-box service can Message the OTP to user
 	}
 
-	@RequestMapping(value = "/getNearByShops", method = RequestMethod.GET)
+	@RequestMapping(value = "/signUp", method = RequestMethod.POST)
 	public Map<String, String> getAllNearByShops(@RequestParam String lat, @RequestParam String lng){
 		Map<String, String> shopMap=new HashMap<String, String>();
 		shopMap.put("Shop1", "My First Shop");
@@ -29,13 +37,8 @@ public class TCPController {
 		return shopMap;	
 	}
 	
-	@RequestMapping(value = "/getShop", method = RequestMethod.GET)
+	@RequestMapping(value = "/signIN", method = RequestMethod.POST)
 	public String getShopDetails(@RequestParam long shopID) {
 		return "The shpo name is Shop zxcv and ShopID is "+ shopID;
 	}
-	
-	//@RequestMapping(value = "/createShop", method = RequestMethod.POST)
-	//public Map<String, String> createShopWithoutMenu(@RequestBody ShopWithoutMenu swom){
-		
-	//}
 }
